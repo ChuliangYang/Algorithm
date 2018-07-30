6f5bd73ee3f3c3f9269922ede154ac08ebfc3e15
CarlosYang <mrcarlosyang@gmail.com>
2018-07-29 21:29:08 -0400

    plus one

--- Java/src/main/java/com/me/cl/java/array/Solution.java
+++ Java/src/main/java/com/me/cl/java/array/Solution.java
@@ -152,4 +152,74 @@ public class Solution {
         return -1;
 
     }
+
+//    Plus One
+//    Go to Discuss
+//    Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+//
+//    The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+//
+//    You may assume the integer does not contain any leading zero, except the number 0 itself.
+//
+//            Example 1:
+//
+//    Input: [1,2,3]
+//    Output: [1,2,4]
+//    Explanation: The array represents the integer 123.
+//    Example 2:
+//
+//    Input: [4,3,2,1]
+//    Output: [4,3,2,2]
+//    Explanation: The array represents the integer 4321.
+
+    public int[] plusOne(int[] digits) {
+        boolean carry=false;
+        boolean addFirst=false;
+        digits[digits.length-1]+=1;
+        if(digits[digits.length-1]>=10){
+            carry=true;
+            digits[digits.length-1]-=10;
+            int index=digits.length-2;
+            while(carry){
+                if(index<0){
+                    addFirst=true;
+                    break;
+                }else{
+                    digits[index]++;
+                    carry=false;
+                    if(digits[index]>=10){
+                        digits[index]-=10;
+                        carry=true;
+                    }
+                }
+                index--;
+            }
+        }
+        if(addFirst){
+            int[] result=new int[digits.length+1];
+            result[0]=1;
+            System.arraycopy(digits,0,result,1,digits.length);
+            return result;
+        }
+        return digits;
+    }
+
+//     public int[] plusOne(int[] digits) {
+
+//     int n = digits.length;
+//     for(int i=n-1; i>=0; i--) {
+//         if(digits[i] < 9) {
+//             digits[i]++;
+//             return digits;
+//         }
+
+//         digits[i] = 0;
+//     }
+
+//     int[] newNumber = new int [n+1];
+//     newNumber[0] = 1;
+
+//     return newNumber;
+//}
+
 }
