010cd8078582d7871ff37d846138edf7ae0deb1e
CarlosYang <mrcarlosyang@gmail.com>
2018-10-09 19:48:01 -0400

    add new

--- Java/src/main/java/com/me/cl/java/array/BestTimeSellStockII309.java
+++ Java/src/main/java/com/me/cl/java/array/BestTimeSellStockII309.java
@@ -1,47 +1,45 @@
 package com.me.cl.java.array;
 
 /**
- Say you have an array for which the ith element is the price of a given stock on day i.
-
- Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
-
- You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
- After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
- Example:
-
- Input: [1,2,3,0,2]
- Output: 3
- Explanation: transactions = [buy, sell, cooldown, buy, sell]
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * <p>
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+ * <p>
+ * You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ * After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+ * Example:
+ * <p>
+ * Input: [1,2,3,0,2]
+ * Output: 3
+ * Explanation: transactions = [buy, sell, cooldown, buy, sell]
  */
 public class BestTimeSellStockII309 {
-        private int[] prices;
-        private int maxProfit=0;
-        public int maxProfit(int[] prices) {
-            this.prices=prices;
-            transaction(-1,-1,0);
-            return maxProfit;
+    public int maxProfit(int[] prices) {
+        boolean keep = false;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (i == prices.length - 1) {
+                if (keep) {
+                    profit += prices[i];
+                }
+                break;
+            }
+            if (prices[i + 1] > prices[i]) {
+                if (!keep) {
+                    keep = !keep;
+                    profit -= prices[i];
+                }
+            }
+            if (prices[i + 1] < prices[i]) {
+                if (keep) {
+                    keep = !keep;
+                    profit += prices[i];
+                }
+            }
         }
+        return profit;
+    }
 
-        public void transaction(int dayIndex, int operation, int totalProfit){
-            if(dayIndex!=-1){
-                totalProfit+=prices[dayIndex]*operation;
-                if(dayIndex==prices.length-1){
-                    maxProfit=Math.max(totalProfit,maxProfit);
-                    return;
-                }
-                if(operation==1){
-                    dayIndex++;
-                }
-                operation=-operation;
-            }
-            for(int i=dayIndex+1;i<=prices.length;i++){
-                if(i==prices.length){
-                    maxProfit=Math.max(totalProfit,maxProfit);
-                    return;
-                }
-                transaction(i,operation,totalProfit);
-            }
-        }
 }
 
 
