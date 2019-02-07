aaab31f6d87b0a953cc3686de5faaca0aa5dbfef
CarlosYang <mrcarlosyang@gmail.com>
2019-02-06 22:30:56 -0500

    add new

---
+++ Java/src/main/java/com/me/cl/java/test.java
@@ -0,0 +1,9 @@
+package com.me.cl.java;
+
+import com.me.cl.java.string.StrobogrammaticNumber246;
+
+public class test {
+    public static void main(String[] args){
+       System.out.println(new StrobogrammaticNumber246().isStrobogrammatic("69"));
+    }
+}
